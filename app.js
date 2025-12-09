2a6537c2ed6c2b2aa6cbf714228faf05c0617426
adikumaran <adi.kumaran05@gmail.com>
2025-12-09 00:10:47 -0500

    Remove debug lines in app.js

--- app.js
+++ app.js
@@ -86,14 +86,3 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
-
-
-
-/*
-Testing
-*/
-
-import { searchHospitalByName, getHospitalsThroughFiter } from "./data/hospitals.js"; 
-
-const hospitalList = await getHospitalsThroughFiter({facility_type: "ambulatory care facility", county: "SOMERset", isActive: true});
-console.log(hospitalList);
